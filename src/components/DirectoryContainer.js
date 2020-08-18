96aeb27546901d09502a1ea3de83fadef68798bc
Aaron <aaronkh19@gmail.com>
2020-08-18 13:39:39 -0600

    added employeeCard

--- src/components/DirectoryContainer.js
+++ src/components/DirectoryContainer.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
-import API from "../utils/API";
+import API from "../utils/API.js";
 
 class DirectoryContainer extends Component {
   state = {
     result: [],
-    search: "",
-    sortType: "asc",
+    filter: "",
+    filterBy: "lastName",
+    currentSort: "default",
+    sortField: "",
   };
 
-  userSearch = () => {
+  //   userSearch = () => {
+  //     API.search()
+  //       .then((res) => {
+  //         this.setState({ result: res.data.results });
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
+
+  componentDidMount() {
     API.search()
       .then((res) => {
-        this.setState({ result: res.data.results });
+        console.log(res);
+        this.setState({
+          result: res.data.results.map((e, i) => ({
+            firstName: e.name.first,
+            lastName: e.name.last,
+            picture: e.picture.large,
+            email: e.email,
+            phone: e.phone,
+            dob: e.age,
+            key: i,
+          })),
+        });
       })
       .catch((err) => console.log(err));
-  };
+  }
 }
 
 export default DirectoryContainer;
